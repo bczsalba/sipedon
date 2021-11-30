a800bfeb78203405bff78d63541bec1536550e7f
bczsalba <bczsalba@gmail.com>
2021-11-30 23:14:44 +0100

    Rename project `fishtank` -> `sipedon`
    The latin name of the Common Watersnake is `Nerodia Sipedon`. May not be
    a great name, but I personally like how it sounds. It is subject to
    change though.

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="fishtank",
+    name="sipedon",
     version="0.0.0",
     include_package_data=True,
-    packages=["fishtank"],
+    packages=["sipedon"],
     license="MIT",
     description="",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     install_requires=["pytermgui"],
     python_requires=">=3.7.0",
-    url="https://github.com/bczsalba/fishtank",
+    url="https://github.com/bczsalba/sipedon",
     author="BcZsalba",
     author_email="bczsalba@gmail.com",
-    entry_points={"console_scripts": ["fishtank = fishtank:main"]},
+    entry_points={"console_scripts": ["sipedon = sipedon:main"]},
 )
